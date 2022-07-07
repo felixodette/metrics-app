8d953654cb038cf5c860e53370e90c8adcf4f44b
Felix Odete <felix.odette@gmail.com>
2022-07-07 05:46:42 +0300

    Update Country.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Header from '../components/Header';
+import Country from '../components/Country';
+import jp from '../data/maps/jp.png';
 
 const Home = () => (
   <>
     <Header heading="Weather" />
-    <div>Home</div>
+    <section>
+      <div>Home</div>
+      <div className="countries">
+        <Country number={4} name="Japan" vector={jp} />
+        <Country number={4} name="Japan" vector={jp} />
+        <Country number={4} name="Japan" vector={jp} />
+        <Country number={4} name="Japan" vector={jp} />
+      </div>
+    </section>
   </>
 );
 
