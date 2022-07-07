258b67ced847469b6b27671b14d4d8fb0feceba3
Felix Odete <felix.odette@gmail.com>
2022-07-07 07:37:41 +0300

    Fix country ke

--- src/redux/countries/countries.js
+++ src/redux/countries/countries.js
@@ -3,7 +3,7 @@ import countries from '../../data/countries';
 const filterCountries = (value) => ({ type: 'Filter', value });
 const initialState = countries;
 
-const updateCountries = (state = initalState, action) => {
+const updateCountries = (state = initialState, action) => {
   switch (action.type) {
     case 'Filter':
       return initialState.filter((country) => country.region === action.value);
