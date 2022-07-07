a5fe15b1bf382040c156728e370b94b983ef2a44
Felix Odete <felix.odette@gmail.com>
2022-07-07 06:54:21 +0300

    Add option region filter

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,20 +2,61 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Heading.module.css';
 
-function Heading(props) {
-  const { title, number } = props;
+const Heading = (props) => {
+  const { title, number, home } = props;
+  const regions = [
+    {
+      name: 'Africa',
+      value: 'africa',
+    },
+    {
+      name: 'Asia',
+      value: 'asia',
+    },
+    {
+      name: 'Europe',
+      value: 'europe',
+    },
+    {
+      name: 'North America',
+      value: 'north-america',
+    },
+    {
+      name: 'Oceania',
+      value: 'oceania',
+    },
+    {
+      name: 'South America',
+      value: 'south-america',
+    },
+  ];
+
   return (
     <div className={styles.heading}>
       <h1>{title}</h1>
-      <span>{number}</span>
+      {
+        (home) ? (
+          <select className={styles.filter}>
+            <option selected disabled>__Filter Countries by Region__</option>
+            {regions.map((region) => (
+              <option key={region.value} value={region.value}>{region.name}</option>
+            ))}
+          </select>
+        ) : (
+          <>
+            <span>{number}</span>
+          </>
+        )
+      }
     </div>
   );
-}
+};
 
 export default Heading;
 
 Heading.propTypes = {
   title: PropTypes.string.isRequired,
+  home: PropTypes.bool,
   number: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
@@ -24,4 +65,5 @@ Heading.propTypes = {
 
 Heading.defaultProps = {
   number: '',
+  home: false,
 };
