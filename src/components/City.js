25faf3f99b2d2f796ece854407065acfc5f907ce
Felix Odete <felix.odette@gmail.com>
2022-07-08 11:56:11 +0300

    Add City reducer

--- src/components/City.js
+++ src/components/City.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import styles from './City.module.css';
+import { fetchWeather } from '../redux/weather/weather';
 
 const City = (props) => {
-  const { title } = props;
+  const { title, lat, long } = props;
+  const dispatch = useDispatch();
 
   return (
-    <Link to="/details" className={styles.city}>
+    <Link onClick={() => dispatch(fetchWeather({ lat, long }))} to="/details" className={styles.city}>
       <span>{title}</span>
       <div className={styles.info}>
-        <span>AQI: 5</span>
+        <span>AQI: 1</span>
         <i className="right_arrow" />
       </div>
     </Link>
@@ -21,4 +24,6 @@ export default City;
 
 City.propTypes = {
   title: PropTypes.string.isRequired,
+  lat: PropTypes.string.isRequired,
+  long: PropTypes.string.isRequired,
 };
